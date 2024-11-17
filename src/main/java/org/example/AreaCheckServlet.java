95b4e95b002e231908d8f9c1b8735030cf8e96bb
Sinchi <goshat17@gmail.com>
2024-11-17 19:21:47 +0300

    Started working

---
+++ src/main/java/org/example/AreaCheckServlet.java
@@ -0,0 +1,7 @@
+package org.example;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+@WebServlet(name = "AreaCheckServlet", value = "/check")
+public class AreaCheckServlet extends HttpServlet {
+}
