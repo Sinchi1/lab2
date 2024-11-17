38500636fdbd5dfb4518fb77ab0f583bc6576f9a
Sinchi <goshat17@gmail.com>
2024-11-17 19:42:30 +0300

    check almost done, just need to make sure set areas are correct

--- src/main/java/org/example/AreaCheckServlet.java
+++ src/main/java/org/example/AreaCheckServlet.java
@@ -1,7 +1,47 @@
 package org.example;
 
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
 @WebServlet(name = "AreaCheckServlet", value = "/check")
 public class AreaCheckServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        ServletContext context = getServletContext();
+        double x = Double.parseDouble((String) context.getAttribute("x"));
+        double y = Double.parseDouble((String) context.getAttribute("y"));
+        double r = Double.parseDouble((String) context.getAttribute("r"));
+        boolean isHit = (checkCircle(x,y,r) || checkTriangle(x,y,r) || checkSquare(x,y,r));
+
+        context.setAttribute("res", String.valueOf(isHit));
+        try {
+            resp.sendRedirect(req.getContextPath() + "/res.jsp");
+        } catch (IOException e) {
+            e.printStackTrace();
+            resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Каким то образом случилась ошибка отправки результата");
+        }
+    }
+
+
+    // Поправить формулы
+    public boolean checkCircle(double x, double y, double r) {
+        return (((x*x+y*y)<=Math.pow((r),2) && (x<=0)) && (y>=0));
+    }
+
+
+    public boolean checkTriangle(double x, double y, double r) {
+        return (( (y > -x-r) && (y <= 0) && (x <= 0) ));
+    }
+
+
+    public boolean checkSquare(double x, double y, double r) {
+        return (( 0<=x && x<=r/2) &&(0<=y && y<=r));
+    }
 }
--- src/main/java/org/example/ControlServlet.java
+++ src/main/java/org/example/ControlServlet.java
@@ -1,9 +1,9 @@
 package org.example;
 
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet(name = "ControlServlet", value = "/control")
 public class ControlServlet extends HttpServlet {
